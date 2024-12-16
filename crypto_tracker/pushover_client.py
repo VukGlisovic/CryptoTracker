3fd2bca7b0bfdb85ab59b779f1f6810b41469430
Vuk Glisovic <v.glisovic@fourthline.com>
2024-12-16 20:18:18 +0100

    updated pushover client

--- crypto_tracker/pushover_client.py
+++ crypto_tracker/pushover_client.py
@@ -1,23 +1,33 @@
-import json
+import logging
+from datetime import datetime
+
 import requests
+from dateutil.relativedelta import relativedelta
+
+from crypto_tracker.utils import load_config
+
+
+logger = logging.getLogger(__name__)
 
 
 class Pushover:
 
-    def __init__(self):
-        self.config = self.load_config()
+    def __init__(self, cooldown):
+        self.cooldown = cooldown
 
-    @staticmethod
-    def load_config():
-        with open("pushover_config.json", "r") as f:
-            config = json.load(f)
-        return config
+        self.config = load_config("configs/pushover_config.yaml")
+        self.last_message_sent = datetime.now() - relativedelta(seconds=cooldown)  # init sothat a message can be sent immediately
 
-    def send_message(self, message):
+    def send_message(self, message: str) -> None:
+        if datetime.now() < self.last_message_sent + relativedelta(seconds=self.cooldown):
+            # too soon to send another message
+            return
         response = requests.post("https://api.pushover.net/1/messages.json", data={
-            "token": self.config["APP_TOKEN"],
-            "user": self.config["USER_KEY"],
+            "token": self.config["app_token"],
+            "user": self.config["user_key"],
             "message": message
         })
-        print(response.status_code)
-        print(response.text)
+        self.last_message_sent = datetime.now()
+
+        if response.status_code != 200:
+            logger.error(f"Got status code: {response.status_code} with message:\n{response.text}")
