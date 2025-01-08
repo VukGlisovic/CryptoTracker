b1292e0832830454e035021e64a52c458272ebd4
Vuk Glisovic <v.glisovic@fourthline.com>
2025-01-08 22:21:06 +0100

    created retry decorator

--- crypto_tracker/utils.py
+++ crypto_tracker/utils.py
@@ -1,7 +1,40 @@
+import time
+import logging
+from functools import wraps
+
 import yaml
+
+
+logger = logging.getLogger(__name__)
 
 
 def load_config(path: str) -> dict:
     with open(path, "r") as f:
         config = yaml.safe_load(f)
     return config
+
+
+def retry(max_retries: int = 3, backoff_factor: float = 2., exceptions: tuple = (Exception,)):
+    """Decorator to retry a function with exponential backoff.
+    """
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            retries = 0
+            delay = 1
+            while retries <= max_retries:
+                try:
+                    return func(*args, **kwargs)
+                except exceptions as e:
+                    print(f"Attempt {retries + 1} failed: {e}")
+                    if retries == max_retries:
+                        raise  # Reraise the exception after max retries
+                    time.sleep(delay)
+                    retries += 1
+                    delay *= backoff_factor
+            return None  # Return None if all retries fail
+
+        return wrapper
+
+    return decorator
